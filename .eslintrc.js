d1c76e44899bfe337ce35716ea3ea48ad6ed17ae
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-01-16 23:34:37 +0200

    Add formatting and linting config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
+  plugins: ['simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'off', // TODO: change to error when this is solved https://github.com/vercel/next.js/issues/5533
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton']
+      }
+    ],
+    'jsx-a11y/anchor-has-content': 'off', // TODO: change to error when this is solved https://github.com/vercel/next.js/issues/5533
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  }
+};
