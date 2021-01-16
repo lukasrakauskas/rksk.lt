41e1ee971ecc0d2c9e4a493e99b22ec9602e281a
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-01-16 23:57:55 +0200

    Add import and absolute path settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,28 @@ module.exports = {
     ],
     'jsx-a11y/anchor-has-content': 'off', // TODO: change to error when this is solved https://github.com/vercel/next.js/issues/5533
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-    'simple-import-sort/imports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)'
+          ],
+          // Packages. `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(src|@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$']
+        ]
+      }
+    ],
     'simple-import-sort/exports': 'error'
   }
 };
