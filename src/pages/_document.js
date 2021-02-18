724a1c85b23caf246bde68d79967047417372375
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-02-18 19:57:56 +0200

    Remove google fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,21 +1,15 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import GoogleFonts from 'next-google-fonts';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
         <Head>
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com/"
-            crossOrigin=""
-          />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
           <meta content="#ffffff" name="theme-color" />
           <meta content="#ffffff" name="msapplication-TileColor" />
         </Head>
-        <body className="antialiased bg-white dark:bg-black text-white dark:text-black">
+        <body className="antialiased text-white bg-white dark:bg-black dark:text-black">
           <Main />
           <NextScript />
         </body>
