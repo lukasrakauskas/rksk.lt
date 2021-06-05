a18e95af0ba4d1b21c972ac4ed5f4ab3bc361ecd
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-06-05 20:48:31 +0300

    Use deta base to store messages

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,23 +1,20 @@
-const mail = require('@sendgrid/mail');
-mail.setApiKey(process.env.SENDGRID_API_KEY);
+import { Deta } from 'deta';
+
+const deta = Deta(process.env.DETA_ACCESS_KEY);
+const db = deta.Base('messages');
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
-    if (!req.body.name || !req.body.email || !req.body.message) return;
+  if (req.method !== 'POST') return res.status(500).json({});
+  if (!req.body.name || !req.body.email || !req.body.message)
+    return res.status(500).json({});
 
-    const message = {
-      to: 'lukas@rksk.lt',
-      from: 'noreply@rksk.lt',
-      subject: `Message from ${req.body.name} ${req.body.email} on rksk.lt`,
-      text: req.body.message
-    };
-
-    try {
-      await mail.send(message);
-    } catch (error) {
-      return res.status(500).json({});
-    }
+  try {
+    const { name, email, message } = req.body;
+    const created_at = new Date();
+    const toCreate = { name, email, message, created_at };
+    const insertedMessage = await db.put(toCreate);
+    return res.status(201).json(insertedMessage);
+  } catch (error) {
+    return res.status(500).json({});
   }
-
-  return res.status(200).json({});
 };
